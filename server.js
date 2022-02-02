d3d2966951abf61f149da7f09439a5fd1c2681db
rizkietyap <setyakiki76@gmail.com>
2022-02-02 19:38:26 +0700

    first commit

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('db connected'))
+  .catch(e => console.log(e));
+app.use(cors());
+app.use(express.urlencoded({ extended: 'false' }));
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-
-
-
+app.use('/api/users', require('./routes/user'));
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
